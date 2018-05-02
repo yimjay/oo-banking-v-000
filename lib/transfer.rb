fc9ad1db47bd498911b886040ccfd7ea36185ac9
Ji Yim <gsooyim@gmail.com>
2018-05-02 19:04:15 +0000

    automatically backed up by learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -13,7 +13,7 @@ class Transfer
   end
   
   def execute_transaction
-    if valid?
+    if valid? && @sender.balance > @amount
       @sender.balance -= @amount
       @receiver.balance += @amount
       @status = "complete"
