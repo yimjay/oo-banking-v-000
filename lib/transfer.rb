f211d55d66130c59b87563d38802dec8b0ad96da
Ji Yim <gsooyim@gmail.com>
2018-05-02 18:47:42 +0000

    automatically backed up by learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -1,3 +1,22 @@
 class Transfer
-  # your code here
+  attr_accessor :sender, :receiver, :amount, :status
+  
+  def initialize(sender, receiver, amount)
+    @sender = sender
+    @receiver = receiver
+    @amount = amount
+    @status = "pending"
+  end
+  
+  def valid?
+    @sender.status == "open" && @receiver.status == "open"
+  end
+  
+  def execute_transaction
+    if valid?
+  end
+  
+  
+  
+  
 end
